c0e7af0984018df4768fd341bbd52852a5dd153b
davide-unipv <62263184+davide-unipv@users.noreply.github.com>
2020-07-01 11:09:36 +0200

    first committ

---
+++ src/it/unipv/so/Tester.java
@@ -0,0 +1,14 @@
+package it.unipv.so;
+
+public class Tester {
+
+	public Tester() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
